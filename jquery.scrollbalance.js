32432c939d39158b4df8d1ca9941f34c95c4d710
Greg Brown <gregplaysguitar@gmail.com>
2014-02-18 13:33:52 +1300

    Added support for dynamic column content, added some docs and tidied up

--- jquery.scrollbalance.js
+++ jquery.scrollbalance.js
@@ -6,140 +6,148 @@ $('.scrollwrap').scrollbalance({
     childSelector: '.mycolumn'
 });
 
-params
-
-childSelector: 
- 
-topBuffer: the 
-
-threshold: the 
-
-
 
 */
 (function($) {
 
     $.fn.scrollbalance = (function(options){
         var settings = $.extend({
-            // selector to find the child elements of container which should be balanced.
+            // selector to find the child elements of container which should be
+            // balanced.
             childSelector: '.scrollbox',
             
-            // distance to maintain between the top of the stationary element and the top of the container.
+            // distance to maintain between the top of the stationary element 
+            // and the top of the container.
             topBuffer: 0,
             
-            // threshold for activating the plugin, eg the column heights must differ by at least this amount to be affected.
+            // threshold for activating the plugin, eg the column heights must 
+            // differ by at least this amount to be affected.
             threshold: 100,
             
-            // filter for columns which should be pinned to the top, even if they are 
-            // taller than the viewport
+            // filter for columns which should be pinned to the top, even if they
+            // are taller than the viewport
             pinTopFilter: null
         }, options);
 
         this.each(function() {
             var container = $(this);
-            
-            
-            function top() {
-                return container.offset().top;
-            };
-            
-            // make sure container has height
-            container.find(settings.childSelector).each(function() {
-                if (container.height() < $(this).outerHeight(true)) {
-                    container.css('height', $(this).outerHeight(true) + 'px');
-                }
-            });
-            
+
+            // Initial setup
             container.find(settings.childSelector).each(function() {
                 var col = $(this),
-                    colHeight = col.outerHeight(true),
-                    colWidth = col.width(),
-                    maxScroll = container.height() - colHeight;
-                
-                function pinTop() {
-                    return settings.pinTopFilter && col.is(settings.pinTopFilter) || (colHeight < $(window).height());
-                };
-
-                // don't do anything if the columns are too close in height.
-                if (maxScroll > settings.threshold) {
-                    var innerHeight = col.height(),
-                        inner = $('<div>').append(col.children());
-                    
-                    col.html('').append(inner).css({
-                        height: innerHeight + 'px',
-                        position: 'relative'
-                    });
-                    
+                    inner = $('<div>').addClass('scrollbalance-inner')
+                                      .append(col.children());
+            
+                col.html('').append(inner).css({
+                    //height: innerHeight + 'px',
+                    position: 'relative'
+                });
+            });
+            
+            function balance_init() {
+                var height = 0;
+                container.find(settings.childSelector).each(function() {
+                    var col = $(this),
+                        inner = col.find('.scrollbalance-inner');
                     inner.css({
-                        width: colWidth + 'px',
+                        width: col.width() + 'px',
                         position: 'absolute',
                         top: 0,
                         left: 0,
                         paddingLeft: col.css('paddingLeft')
                     });
+                    col.css({
+                        height: inner.outerHeight(true)
+                    });
+                    height = Math.max(height, inner.outerHeight(true));
+                });
+                container.css('height', height + 'px');
+            };
+            balance_init();
+            $(window).resize(balance_init);
+            
+            function top() {
+                return container.offset().top;
+            };
+            
+            function balance(col) {
+                var inner = col.find('.scrollbalance-inner'),
+                    col_height = col.outerHeight(true),
+                    max_scroll = container.height() - col_height,
+                    pin_top = settings.pinTopFilter && col.is(settings.pinTopFilter)
+                                || (col_height < $(window).height());
 
-                    var fixTop, fixLeft;
-                    function balance_init() {
-                        if (pinTop()) {
-                            fixTop = settings.topBuffer;
-                        }
-                        else {
-                            fixTop = $(window).height() - colHeight;
-                        }
-                        fixLeft = col.offset().left + parseInt(col.css('borderLeftWidth'));
-                    };
-                    
-                    function balance() {
-                        if (pinTop()) {
-                            var raw_scroll = $(window).scrollTop() - top() + settings.topBuffer;
-                        }
-                        else {
-                            var raw_scroll = ($(window).height() + $(window).scrollTop()) - (top() + colHeight);
-                        }
-                        var scroll = Math.max(
-                            0,
-                            Math.min(
-                                maxScroll,
-                                raw_scroll
-                            )
-                        );
-
-                        if (scroll && scroll < maxScroll) {
-                            inner.css({
-                                position: 'fixed',
-                                top: fixTop + 'px',
-                                left: fixLeft - $(window).scrollLeft() + 'px'
-                            });
-                        }
-                        else if (scroll) {
-                            inner.css({
-                                position: 'absolute',
-                                top: maxScroll + 'px',
-                                left: 0
-                            });
-                        }
-                        else {
-                            inner.css({
-                                position: 'absolute',
-                                top: 0,
-                                left: 0
-                            });
-
-                        }
-                    };
-                    
-                    balance_init();
-                    balance();
-                    $(window).scroll(balance);
-                    $(window).resize(function() {
-                        balance_init();
-                        balance();
+                if (max_scroll < settings.threshold) {
+                    // scrolling behaves normally if columns are too close in
+                    // height.
+                    inner.css({
+                        position: 'relative',
+                        top: 0,
+                        left: 0
                     });
                 }
+                else {
+                    if (pin_top) {
+                        var raw_scroll = $(window).scrollTop() - top() + 
+                                         settings.topBuffer;
+                    }
+                    else {
+                        var raw_scroll = ($(window).height() + 
+                                          $(window).scrollTop()) - 
+                                          (top() + col_height);
+                    }
+                    var scroll = Math.max(
+                        0,
+                        Math.min(
+                            max_scroll,
+                            raw_scroll
+                        )
+                    );
+            
+                    if (scroll && scroll < max_scroll) {
+                        var fix_top = pin_top ? settings.topBuffer :
+                                        $(window).height() - col_height,
+                            fix_left = col.offset().left + 
+                                       parseInt(col.css('borderLeftWidth'));
+                        inner.css({
+                            position: 'fixed',
+                            top: fix_top + 'px',
+                            left: fix_left - $(window).scrollLeft() + 'px'
+                        });
+                    }
+                    else if (scroll) {
+                        inner.css({
+                            position: 'absolute',
+                            top: max_scroll + 'px',
+                            left: 0
+                        });
+                    }
+                    else {
+                        inner.css({
+                            position: 'absolute',
+                            top: 0,
+                            left: 0
+                        });
+                    }
+                }
+            };
+            
+            function balance_all() {
+                container.find(settings.childSelector).each(function() {
+                    balance($(this));
+                });
+            };
+            balance_all();
+            $(window).scroll(balance_all);
+            $(window).resize(balance_all);
+                        
+            container.data('scrollbalance', {
+                reinitialise: function() {
+                    balance_init();
+                    balance_all();
+                }
             });
-
         });
     });
-        
 
 })(jQuery);
--- readme.md
+++ readme.md
@@ -1,10 +1,57 @@
 # jQuery Scroll Balance
 
 A jQuery plugin that intelligently uses position: fixed to combat unsightly gaps 
-in multi-column layouts, when columns are of different heights. See index.html for
-a demo.
+in multi-column layouts, when columns are of different heights. See index.html 
+for a demo.
 
-Requires jquery – should work with version 1.2.6 or higher. 
+Requires jquery version 1.2.6 or higher. 
+
+## Usage
+
+Assuming you have markup like the following:
+    
+    <div class="wrap">
+        <div class="column">...</div>
+        <div class="column">...</div>
+        <div class="column">...</div>
+    </div>
+
+Initialise the plugin like so:
+
+    $('.wrap').scrollbalance({
+      childSelector: '.column'
+    });
+
+
+### Optional parameters
+
+- topBuffer: distance to maintain between the top of the stationary element 
+             and the top of the container. (default 0)
+- threshold: minimum pixel difference in column heights for the plugin to activate 
+             (default 100)
+- pinTopFilter: jquery filter to identify columns which should be pinned to the top,               
+                even if they are taller than the viewport (default null)
+
+
+### Dynamic content
+
+If your column heights change dynamically, you'll need to reinitialise the plugin.
+For example:
+
+    $('.wrap').scrollbalance({
+      childSelector: '.column'
+    });
+    var api = $('.wrap').data('scrollbalance');
+    
+    // add some content here
+    ...
+    
+    api.reinitialise();
+
+
+### Demo
+
+See index.html for a demo.
 
 ## License
 
